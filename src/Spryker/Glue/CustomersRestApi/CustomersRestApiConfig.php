ec7178ce7c25b51991a599accb40081347ce5af7
René Klatt <rene.klatt@spryker.com>
2026-06-24 10:11:26 +0200

    Updated doc blocks (#581)
    * Updated doc blocks

--- src/Spryker/Glue/CustomersRestApi/CustomersRestApiConfig.php
+++ src/Spryker/Glue/CustomersRestApi/CustomersRestApiConfig.php
@@ -15,301 +15,421 @@ use Symfony\Component\HttpFoundation\Response;
 class CustomersRestApiConfig extends AbstractBundleConfig
 {
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_CUSTOMERS = 'customers';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_ADDRESSES = 'addresses';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_CUSTOMER_PASSWORD = 'customer-password';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_FORGOTTEN_PASSWORD = 'customer-forgotten-password';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_CUSTOMER_RESTORE_PASSWORD = 'customer-restore-password';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_CUSTOMER_CONFIRMATION = 'customer-confirmation';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const CONTROLLER_CUSTOMER_FORGOTTEN_PASSWORD = 'customer-forgotten-password-resource';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const CONTROLLER_CUSTOMER_RESTORE_PASSWORD = 'customer-restore-password-resource';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CUSTOMER_ALREADY_EXISTS = '400';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_MESSAGE_CUSTOMER_ALREADY_EXISTS = 'If this email address is already in use, you will receive a password reset link. Otherwise you must first validate your e-mail address to finish registration. Please check your e-mail.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CUSTOMER_CANT_REGISTER_CUSTOMER = '401';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_MESSAGE_CUSTOMER_CANT_REGISTER_CUSTOMER = 'Can\'t register a customer.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CUSTOMER_NOT_FOUND = '402';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAILS_CUSTOMER_NOT_FOUND = 'Customer not found.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CUSTOMER_ADDRESSES_NOT_FOUND = '403';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAILS_CUSTOMER_ADDRESSES_NOT_FOUND = 'Customer does not have addresses.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_ADDRESS_NOT_FOUND = '404';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAILS_ADDRESS_NOT_FOUND = 'Address was not found.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CUSTOMER_REFERENCE_MISSING = '405';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAILS_CUSTOMER_REFERENCE_MISSING = 'Customer reference is missing.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_PASSWORDS_DONT_MATCH = '406';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAILS_PASSWORDS_DONT_MATCH = 'Value in field %s should be identical to value in the %s field.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_PASSWORD_CHANGE_FAILED = '407';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAILS_PASSWORD_CHANGE_FAILED = 'Failed to change password.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_INVALID_PASSWORD = '408';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAILS_INVALID_PASSWORD = 'Invalid password';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CUSTOMER_ADDRESS_FAILED_TO_SAVE = '409';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAILS_CUSTOMER_ADDRESS_FAILED_TO_SAVE = 'Failed to save customer address.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CUSTOMER_FAILED_TO_SAVE = '410';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAILS_CUSTOMER_FAILED_TO_SAVE = 'Failed to save customer.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CUSTOMER_UNAUTHORIZED = '411';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAILS_CUSTOMER_UNAUTHORIZED = 'Unauthorized request.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_ADDRESS_UUID_MISSING = '412';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAILS_ADDRESS_UUID_MISSING = 'Address UUID is missing.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_NOT_ACCEPTED_TERMS = '413';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAILS_NOT_ACCEPTED_TERMS = 'Terms and Conditions was not accepted.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_NOT_VALID_GENDER = '414';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAILS_NOT_VALID_GENDER = 'Gender is not valid.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_RESTORE_PASSWORD_KEY_INVALID = '415';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_DETAILS_RESTORE_PASSWORD_KEY_INVALID = 'Restore password key is not valid.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CUSTOMER_EMAIL_INVALID = '416';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_MESSAGE_CUSTOMER_EMAIL_INVALID = 'Invalid Email address format.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CUSTOMER_EMAIL_LENGTH_EXCEEDED = '417';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_MESSAGE_CUSTOMER_EMAIL_LENGTH_EXCEEDED = 'Email is too long. It should have 100 characters or less.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CUSTOMER_PASSWORD_TOO_SHORT = '418';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_MESSAGE_CUSTOMER_PASSWORD_TOO_SHORT = 'The password is too short.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CUSTOMER_PASSWORD_TOO_LONG = '419';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_MESSAGE_CUSTOMER_PASSWORD_TOO_LONG = 'The password is too long.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CUSTOMER_PASSWORD_INVALID_CHARACTER_SET = '420';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_MESSAGE_CUSTOMER_PASSWORD_INVALID_CHARACTER_SET = 'The password character set is invalid.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CUSTOMER_PASSWORD_SEQUENCE_NOT_ALLOWED = '421';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_MESSAGE_CUSTOMER_PASSWORD_SEQUENCE_NOT_ALLOWED = 'The password contains sequence of the same character.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CUSTOMER_PASSWORD_DENY_LIST = '422';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_MESSAGE_CUSTOMER_PASSWORD_DENY_LIST = 'The password is listed as common.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CONFIRMATION_CODE_INVALID = '423';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_MESSAGE_CONFIRMATION_CODE_INVALID = 'This email confirmation code is invalid or has been already used.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CONFIRMATION_CODE_MISSING = '424';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_MESSAGE_CONFIRMATION_CODE_MISSING = 'Token is invalid.';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_CONFIRMATION_FAILED = '425';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_MESSAGE_CONFIRMATION_FAILED = 'Failed to confirm a customer.';
@@ -341,6 +461,8 @@ class CustomersRestApiConfig extends AbstractBundleConfig
     }
 
     /**
+     * @api
+     *
      * @deprecated Will be removed without replacement.
      *
      * @var string
